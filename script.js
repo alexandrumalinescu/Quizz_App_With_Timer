8bb0a547aa3634cb27b868a2ea4a450928a1eaf2
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-02-06 14:24:06 +0200

    time update

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
+const daysEl = document.getElementById('days')
+const hoursEl = document.getElementById('hours')
+const minsEl = document.getElementById ('minutes')
+const secondsEl = document.getElementById ('seconds')
+
 const timer = '1 Jan 2022';
 
 function countdown() {
   const newYearDate = new Date(timer);
   const currentDate = new Date();
 
-  const diff = (newYearDate - currentDate)/1000;
-  const days = Math.floor(seconds / 3600 / 24);
-  const hours =Math.floor(seconds / 3600) % 24;
+  const totalSeconds = (newYearDate - currentDate)/1000;
+  
+  //const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60)  % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
 
-  console.log(days);
+
+
+hoursEl.innerHTML= hours;
+minsEl.innerHTML= minutes
+secondsEl.innerHTML= seconds
+
 }
+
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+
 
 countdown();
 
 setInterval(countdown, 1000);
+
